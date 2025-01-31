121b848957e46cac75f720bfb8e1961b01fa2084
Ahmed Roshdy Ragheb Mohamed <ahmedroshdy6555554@gmail.com>
2025-01-31 16:38:30 +0200

    Commit removing Dio and adding Http, Adding comments

--- lib/services/employee_service.dart
+++ lib/services/employee_service.dart
@@ -1,16 +1,17 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:employees_data/model/employee_model.dart';
 
 class EmployeeService {
-  String endpoint =
-    "https://jsonplaceholder.typicode.com/users";
-  final dio = Dio();
-// function to parse the json response into list
+  String endpoint ="https://jsonplaceholder.typicode.com/users";
+
+// function using Http to get the json response into list
   Future<List<Employee>> getEmployees() async {
     List<Employee> employee = [];
     try {
-      final response = await dio.get(endpoint);
-      var data= response.data;
+      final response = await http.get(Uri.parse(endpoint));
+      var data= jsonDecode(response.body);
       data.forEach((json) {
         Employee user = Employee.fromJson(json);
         employee.add(user);
